187f3b647ec915989cdf3a6b07ff3bf1d901f2c8
Diogo <diogo.rs2005@aluno.ifsc.edu.br>
2024-03-27 11:25:08 -0300

    Reiniciando projeto

--- libprg/src/include/libprg/libprg.h
+++ libprg/src/include/libprg/libprg.h
@@ -1,15 +1,34 @@
+//
+// Created by aluno on 13/03/24.
+//
+
 #ifndef LIBPRG_LIBPRG_H
 #define LIBPRG_LIBPRG_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+//structs
+typedef struct fila fila_t;
 
-typedef struct {
-    double value;
-    int error;
-} result_t;
 
-typedef enum { SUM, SUB } operation_t;
+//lista
+void criarlista(int **vetor, int tam);
+void povoar(int *vetor, int tam);
 
-result_t sub(double a, double b);
-result_t sum(double a, double b);
-result_t compute(double a, double b, operation_t op);
 
+int buscalinear(int *vetor,int tam, int elem);
+int bulisentinela(int *vetor, int tam, int elem);
+int bulisentinelaord(int *vetor, int tam, int elem);
+
+int buscabinariaite(int *vetor, int tam, int elem);
+int buscabinariarec(int *vetor, int ini, int fim, int elem);
+//fila
+fila_t *fila criarfila(int capacidade);
+bool enfileirar(fila_t *fila, int elemento);
+int desinfileirar(fila_t *fila);
+int getinicio(fila_t *fila);
+int getfinal(fila_t *fila);
+int gettamanho(fila_t *fila);
+bool isvazia(fila_t *fila);
+bool ischeia(fila_t *fila);
 #endif
---
+++ libprg/src/libprg/fila.c
@@ -0,0 +1,64 @@
+#include <libprg/libprg.h>
+
+typedef struct fila{
+    int *vetor;
+    int inicio;
+    int fim;
+    int tamanho;
+    int capacidade;
+} fila_t;
+
+fila_t *fila criarfila(int capacidade){
+    *fila->vetor = (int*) calloc(capacidade, sizeof(int));
+    *fila->inicio = 0;
+    *fila->fim = 0;
+    *fila->tamanho = 0;
+    *fila->capacidade = capacidade;
+    return fila;
+}
+bool ischeia(fila_t *fila){
+    if (fila->tamanho == fila->capacidade{return true;}
+    return false;
+}
+bool isvazia(fila_t *fila){
+    if (fila->tamanho == 0){return true;}
+    return false;
+}
+
+int getinicio(fila_t *fila){
+    return fila->vetor[fila->inicio];
+}
+int getfinal(fila_t *fila){
+    return fila->vetor[fila->fim-1];
+}
+int gettamanho(fila_t *fila){
+    return fila->tamanho;
+}
+bool enfileirar(fila_t *fila, int elemento){
+    bool full = cheia(fila);
+    if (full != true){
+        fila->vetor[fila->fim] = elemento;
+        fila->fim++;
+        if (fila->fim > fila->capacidade){fila->fim = 0;}
+        fila->tamanho++;
+        return true;
+    }
+    return false;
+}
+int desinfileirar(fila_t *fila){
+    bool empty = vazia(fila);
+    if (empty != true){
+        fila->inicio++;
+        if (fila->inicio > fila->capacidade){
+            fila->inicio = 0
+            fila->tamanho--;
+            return fila->vetor[fila->capacidade];
+
+        ;}
+        fila->tamanho--;
+        return fila->vetor[fila->inicio-1];
+    }
+    printf("A fila ja esta vazia\n")
+    return -1;
+}
+
